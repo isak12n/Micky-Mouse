854d5a2c1ae6b671ec3b84a2201b50d314f3b23b
Isak <44029040+isak12n@users.noreply.github.com>
2019-09-23 01:25:04 +0200

    Add files via upload
    it's not complete

---
+++ new.js
@@ -0,0 +1,61 @@
+// an object 
+
+// let isak = {
+//     Name: 'isak',
+//     Age: 23,
+//     Heidth: 3.7,
+//     Hobbis: `Football Gym Pingpong`,
+//     Movies: `Comic Marvel DC`
+// }
+
+// isak.Name = 'nazari'
+// console.log(isak.Name);
+
+// array
+
+// let color = ['blue','green','white']
+
+// color[3] = 'black'
+
+// console.log(color.length);
+
+//function
+
+// function game(name){
+
+//     console.log('hello '+ name);
+// }
+// game('isak');
+
+// function name(number){
+//    return number + number;
+// }
+// console.log(name(2));
+
+// let name = (number) => {
+//    return number * number;
+// }
+// console.log(name(2));
+
+
+
+//exemple
+
+// function name(number){
+//    return number * number;
+// }
+// console.log(name(2));
+
+// let obj= {
+//    Name:'isak',
+//    Age:23,
+//    skills: `js html css`,
+//    pets: 'none'
+// }
+// obj.Name='nazari'
+// console.log(obj);
+
+// let arr=['isak','nazari','afghan', 'mersin'];
+// arr[0] = 'khan'
+// console.log(arr)
+
